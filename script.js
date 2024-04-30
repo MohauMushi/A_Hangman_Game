b36beaacfa2add44ac074bc0aa36643037df84cb
Mohau Mushi <david1mushi1@gmail.com>
2024-04-30 15:47:30 +0200

    Declared Variables and added function resetting all game variables and UI elements

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const guessesText = document.querySelector('.guesses-text b');
 const keyboardDiv = document.querySelector('.keyboard');
 const gameModal = document.querySelector('.game-modal');
 const playAgainBtn = document.querySelector('.play-again');
+
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuesses = 6;
+
+const resetGame = () => {
+	// Ressetting all game variables and UI elements
+	correctLetters = [];
+	wrongGuessCount = 0;
+	hangmanImage.src = `./images/hangman-${wrongGuessCount}.svg`;
+	guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+	keyboardDiv.querySelectorAll('button').forEach(btn => btn.disabled = false);
+	wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+	gameModal.classList.remove('show');
+}
