34053cc128a2ed8f6e5c5babb0f17a7432bb7e15
Mohau Mushi <david1mushi1@gmail.com>
2024-04-30 15:45:55 +0200

    selected HTML elements and stored them in variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const hangmanImage = document.querySelector('.hangman-box img');
+const wordDisplay = document.querySelector('.word-display');
+const guessesText = document.querySelector('.guesses-text b');
+const keyboardDiv = document.querySelector('.keyboard');
+const gameModal = document.querySelector('.game-modal');
+const playAgainBtn = document.querySelector('.play-again');
