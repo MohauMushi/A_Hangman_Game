4baaf54f771e3641cb9ee51499e4144f589e737b
Mohau Mushi <david1mushi1@gmail.com>
2024-04-30 15:54:24 +0200

    created keyboard Buttons using for loop

--- script.js
+++ script.js
@@ -62,3 +62,11 @@ const initGame = (button, clickedLetter) => {
 	if(wrongGuessCount === maxGuesses) return gameOver(false);
 	if(correctLetters.length === currentWord.length) return gameOver(true);
 }
+
+// Creating Keyboard Buttons
+for (let i = 97; i <= 122; i++) {
+	const button = document.createElement('button');
+	button.innerText = String.fromCharCode(i)
+	keyboardDiv.appendChild(button);
+	button.addEventListener('click',  e => initGame(e.target, String.fromCharCode(i)));
+}
